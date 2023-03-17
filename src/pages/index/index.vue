35a0f5ac4aaa9f43d48454f3e59066ac7411d598
chenchao <chenchao@sinodata.com.cn>
2023-03-17 13:40:34 +0800

    Frist Commit

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,155 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="container">
+    <view class="coustom-step">
+      <view class="coustom-step-item" v-for="item in steps" :class="{ 'coustom-step-item_active': currentStep === item.index }" :key="item.index" @click="currentStep = item.index">
+        <view class="item-before"></view>
+        <view class="item-name">{{ item.name }}</view>
+        <view class="item-after"></view>
+      </view>
+    </view>
+
+    <view class="next" @click="handToNext">Next >></view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      steps: [
+        {
+          index: 0,
+          name: '第一步'
+        },
+        {
+          index: 1,
+          name: '第二步'
+        },
+        {
+          index: 2,
+          name: '第三步'
+        },
+        {
+          index: 3,
+          name: '第四步'
+        }
+      ],
+      currentStep: 0
+    }
+  },
+  methods: {
+    handToNext() {
+      uni.navigateTo({
+        url: '/pages/other/index'
+      })
+    }
+  }
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f8f8f8;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+  --default-color: #dfdfdf;
+  --active-color: #5da0ff;
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+  .coustom-step {
+    display: flex;
+    padding: 20rpx 40rpx;
+    box-sizing: border-box;
+    background-color: #fff;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+    &-item {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      position: relative;
+      height: 68rpx;
+      font-size: 24rpx;
+
+      .item-after {
+        flex: 1;
+        width: 95%;
+        height: 34rpx;
+        background-color: var(--default-color);
+        transform: skewX(-30deg);
+      }
+
+      .item-before {
+        width: 95%;
+        height: 34rpx;
+        background-color: var(--default-color);
+        transform: skewX(30deg);
+      }
+
+      .item-name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        color: #8d8d8d;
+        z-index: 1;
+      }
+
+      &:first-child {
+        .item-name {
+          left: -10rpx;
+        }
+
+        .item-before,
+        .item-after {
+          &::before {
+            content: '';
+            position: absolute;
+            width: 34rpx;
+            height: 34rpx;
+            left: -10rpx;
+            background-color: var(--default-color);
+          }
+        }
+
+        .item-before {
+          &::before {
+            transform: skewX(-30deg);
+          }
+        }
+
+        .item-after {
+          &::before {
+            transform: skewX(30deg);
+          }
+        }
+      }
+
+      &_active {
+        .item-before,
+        .item-after {
+          background-color: var(--active-color) !important;
+
+          &::before {
+            background-color: var(--active-color) !important;
+          }
+        }
+
+        .item-name {
+          color: #fff;
+        }
+      }
+    }
+  }
+
+  .next {
+    padding: 20rpx;
+    font-size: 28rpx;
+    color: #5da0ff;
+    text-align: center;
+  }
+}
 </style>
---
+++ src/pages/other/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <view class="container">
+    <view class="coustom-step">
+      <view class="coustom-step-item" v-for="item in steps" :key="item.index" :class="{ 'coustom-step-item_active': currentStep === item.index }" @click="currentStep = item.index">
+        <text class="item-name">{{ item.name }}</text>
+      </view>
+    </view>
+
+    <view class="next">Next >></view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      steps: [
+        {
+          index: 0,
+          name: '第一步'
+        },
+        {
+          index: 1,
+          name: '第二步'
+        },
+        {
+          index: 2,
+          name: '第三步'
+        },
+        {
+          index: 3,
+          name: '第四步'
+        }
+      ],
+      currentStep: 0
+    }
+  },
+  methods: {
+    handToNext() {
+      uni.navigateTo({
+        url: '/pages/index/index'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+
+  .coustom-step {
+    display: flex;
+    padding: 20rpx 48rpx 20rpx 24rpx;
+
+    &-item {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      height: 68rpx;
+      font-size: 24rpx;
+      color: #333;
+      background-color: #f8f8f8;
+
+      .item-name {
+        position: relative;
+        z-index: 2;
+      }
+
+      &::after {
+        content: '';
+        position: absolute;
+        width: 48rpx;
+        height: 48rpx;
+        right: -24rpx;
+        background-color: #f8f8f8;
+        transform: rotate(45deg);
+        z-index: 1;
+        box-shadow: 6rpx -6rpx 2rpx 2rpx #fff;
+      }
+
+      &:not(:first-child) {
+        text-indent: 40rpx;
+      }
+
+      &_active {
+        background-color: #53afff;
+
+        .item-name {
+          color: #fff;
+        }
+
+        &::after {
+          background-color: #53afff;
+        }
+      }
+    }
+  }
+
+  .next {
+    padding: 20rpx;
+    font-size: 28rpx;
+    color: #5da0ff;
+    text-align: center;
+  }
+}
+</style>
